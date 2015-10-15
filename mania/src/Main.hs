5ffc0affc2a9511a2c10a40d00df4981710576c1
raymoo <uguu@installgentoo.com>
2015-10-15 09:12:11 -0700

    Change main to test some features. Need test.ogg in current directory to work.

--- mania/src/Main.hs
+++ mania/src/Main.hs
@@ -1,5 +1,23 @@
 module Main (main) where
 
+import Mania.Sound
+
+import qualified Sound.PortAudio as PA
+
+import Control.Concurrent
+
 
 main :: IO ()
-main = putStrLn "Not functional yet"
+main = do
+  PA.withPortAudio app >> return ()
+
+
+app :: IO (Either a ())
+app = do
+  track <- loadFile "test.ogg"
+  Right (dev,_) <- PA.getDefaultInputInfo
+  playTrackWithCB print (putStrLn "Done!!!!") dev track
+  threadDelay 1000000
+  stopTrack track
+  return $ Right ()
+
