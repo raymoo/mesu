3100b1b3c290cee0017683c96ef5e16c4917710d
raymoo <uguu@installgentoo.com>
2015-11-02 17:12:01 -0800

    Test app for sdl apps

--- mania/src/Main.hs
+++ mania/src/Main.hs
@@ -1,23 +1,54 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import Mania.Sound
+import Mania.Picture
+import Mania.App
 
-import qualified Sound.PortAudio as PA
+import Reflex
 
-import Control.Concurrent
+import qualified SDL as SDL
 
+import Linear
+import Linear.Affine
+
+windowConfig :: SDL.WindowConfig
+windowConfig = SDL.defaultWindow
 
 main :: IO ()
 main = do
-  PA.withPortAudio app >> return ()
+  SDL.initialize [SDL.InitEverything]
+  window <- SDL.createWindow "Mania" windowConfig
+  renderer <- SDL.createRenderer window (-1) $ SDL.RendererConfig SDL.AcceleratedVSyncRenderer True
+
+  loadedBG <- SDL.loadBMP "bg.bmp"
+
+  bg <- SDL.createTextureFromSurface renderer loadedBG
+  
+  runSDLApp renderer (mainApp bg)
+  SDL.quit
 
 
-app :: IO (Either a ())
-app = do
-  track <- loadFile "test.ogg"
-  Right (dev,_) <- PA.getDefaultInputInfo
-  playTrackWithCB print (putStrLn "Done!!!!") dev track
-  threadDelay 1000000
-  stopTrack track
-  return $ Right ()
+posSin :: Floating a => a -> a
+posSin a = (sin a + 1) / 2
 
+
+mainApp :: SDL.Texture -> SDLApp t m
+mainApp tex steps evs = do
+  curPic <- picture
+  return $ SDLDriver curPic (quitEvent evs)
+  where background t =
+          colorFill . fmap floor $ (V4 (posSin (5*t) * 255)  (posSin (3*t - 0.2) * 255) (posSin (17*t + 0.3) * 255) 0)
+        title = textureBox tex (SDL.Rectangle (P $ V2 100 100) (V2 500 400))
+        picture = do
+          timeDyn <- time steps
+          bgDyn <- mapDyn background timeDyn
+          picDyn <- dynList [bgDyn, constDyn title]
+          fmap current $ mapDyn pictures picDyn
+
+dynList :: (Reflex t, MonadHold t m) => [Dynamic t a] -> m (Dynamic t [a])
+dynList dyns = do
+  singleton'd <- traverse (mapDyn (:[])) dyns
+  mconcatDyn singleton'd
+
+                  
