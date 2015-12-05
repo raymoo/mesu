3cfc9946ea0bc911d249fdeaf8db5e1a2a8ec9d3
raymoo <uguu@installgentoo.com>
2015-12-05 13:28:34 -0800

    Widget demo

--- mania/src/Main.hs
+++ mania/src/Main.hs
@@ -4,6 +4,8 @@ module Main (main) where
 
 import Mania.Picture
 import Mania.App
+import Mania.App.Widget.Base
+import Mania.App.Widget.Input
 
 import Reflex
 
@@ -11,6 +13,12 @@ import qualified SDL as SDL
 
 import Linear
 import Linear.Affine
+
+import System.Random
+
+import Control.Monad.Fix
+
+import Data.Word
 
 windowConfig :: SDL.WindowConfig
 windowConfig = SDL.defaultWindow
@@ -21,34 +29,34 @@ main = do
   window <- SDL.createWindow "Mania" windowConfig
   renderer <- SDL.createRenderer window (-1) $ SDL.RendererConfig SDL.AcceleratedVSyncRenderer True
 
-  loadedBG <- SDL.loadBMP "bg.bmp"
-
-  bg <- SDL.createTextureFromSurface renderer loadedBG
-  
-  runSDLApp renderer (mainApp bg)
+  runSDLApp renderer mainApp
   SDL.quit
 
 
-posSin :: Floating a => a -> a
-posSin a = (sin a + 1) / 2
-
-
-mainApp :: SDL.Texture -> SDLApp t m
-mainApp tex AppContext { _appTimeStep = steps, _appSDLEvents = evs } = do
-  curPic <- picture
-  return $ SDLDriver curPic (quitEvent evs)
-  where background t =
-          colorFill . fmap floor $ (V4 (posSin (5*t) * 255)  (posSin (3*t - 0.2) * 255) (posSin (17*t + 0.3) * 255) 0)
-        title = textureBox tex (SDL.Rectangle (P $ V2 100 100) (V2 500 400))
-        picture = do
-          timeDyn <- time steps
-          bgDyn <- mapDyn background timeDyn
-          picDyn <- dynList [bgDyn, constDyn title]
-          fmap current $ mapDyn pictures picDyn
-
-dynList :: (Reflex t, MonadHold t m) => [Dynamic t a] -> m (Dynamic t [a])
-dynList dyns = do
-  singleton'd <- traverse (mapDyn (:[])) dyns
-  mconcatDyn singleton'd
+mainApp :: SDLApp t m
+mainApp context = do
+  (picBeh, _) <- compileWidget context (V2 800 600) colorsWidget
+  return $ SDLDriver picBeh (quitEvent . _appSDLEvents $ context)
 
                   
+randomV4s :: (Num a, Random a, RandomGen g) => g -> [V4 a]
+randomV4s gen = makeTupList (randoms gen)
+  where makeTupList (r:g:b:rest) = V4 r g b 1 : makeTupList rest
+
+
+randomColors :: (Reflex t, MonadHold t m, MonadFix m) =>
+                Int -> Event t a -> m (Event t (V4 Word8))
+randomColors seed event = zipListWithEvent const (randomV4s (mkStdGen seed)) event
+
+
+colorChanging :: (MonadHold t m, Reflex t) =>
+                 V4 Word8 -> Event t (V4 Word8) -> m (Behavior t Picture)
+colorChanging initial colors = (fmap.fmap) colorFill $ hold initial colors
+
+
+colorsWidget :: Reflex t => Widget t ()
+colorsWidget = do
+  datas <- clicks SDL.ButtonLeft
+  randomCols <- randomColors 0 datas
+  curPic <- colorChanging (V4 0 0 0 1) randomCols
+  absolutePicture curPic
